1ba404a0f7e96ed3cc8d99b3039d1f61eee40c86
Amanda Flores <amanda.flores@advendio.com>
2022-10-17 18:12:51 -0300

    modelo item carro

---
+++ models/carro.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var CarroSchema = new mongoose.Schema({
+    mesa:{
+        type:Number,
+        required:true,
+    },
+    carroItems:{
+        type:[],
+        required:false,
+    },
+    metodo_pago:{
+        type:String,
+        required:true,
+    },
+    f_creacion:{
+        type: Date,
+        default: Date.now()
+    },
+    estado:{
+        type:String,
+        required:true,
+    },
+    total:{
+        type:Number,
+        required:false
+    }
+});
+
+//Export the model
+module.exports = mongoose.model('Carro', CarroSchema);
