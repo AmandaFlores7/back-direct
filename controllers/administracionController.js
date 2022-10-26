f093a19bb8f71af26ab6b73ab04d42bfd4da11a3
Amanda Flores <amanda.flores@advendio.com>
2022-10-26 20:25:16 -0300

    user

--- controllers/administracionController.js
+++ controllers/administracionController.js
@@ -1,17 +1,15 @@
 const Administracion = require("../models/administracion");
 
-exports.ObtenerAdministrador = async (req, res) => {
+exports.ObtenerCredencial = async (req, res) => {
     try {
-        let admin = await Administracion.findOne({email: req.body.email});
-        console.log('admin:', admin);
-        if (admin.rol != 'administrador') {
-            res.status(404).json({ msg: 'Error de credencial' })
+        const usuario = await Administracion.findOne({email: req.body.email});
+        //console.log('usuario:', usuario);
+        if (usuario.password != req.body.password) {
+            res.status(404).json({ msg: 'Clave incorrecta' })
         }
-        if (admin.password != req.body.password) {
-            res.status(404).json({ msg: 'Clave incorrectas' })
+        else {
+            res.json(usuario);
         }
-        res.json(admin);
-
     } catch (error) {
         console.log(error);
         res.status(500).send("hubo un error");
