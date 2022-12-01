9d260958f751fe2947ad622d190dd05664ec69f5
Amanda Flores <amanda.flores@advendio.com>
2022-12-01 01:46:28 -0300

    pedido in progress

---
+++ controllers/pedidoController.js
@@ -0,0 +1,41 @@
+const Pedido = require("../models/pedido");
+const carroController = require('../controllers/carroController');
+
+
+exports.crearPedido = async (req, res) => {
+    console.log('pedidiooooo');
+    try {
+        console.log('req: ', req.body.carro);
+        carroController.crearCarro(req.body.carro)
+        // let pedido = new Pedido(req.body);
+        // await pedido.save();
+        // res.send(pedido);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("hubo un error");
+    }
+}
+
+exports.obtenerPedidos = async (req, res) => {
+    try {
+        const pedidos = await Pedido.find();
+        if (pedidos) {
+            res.json(pedidos);
+        }
+        else{
+            res.status(500).send("No existen pedidos");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("hubo un error");
+    }
+}
+
+exports.cambiarEstado = async (req, res) => {
+    try {
+        console.log('req: ', req.body);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("hubo un error");
+    }
+}
