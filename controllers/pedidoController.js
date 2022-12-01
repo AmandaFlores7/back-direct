f9f9953c00ea71e1ee3c1084797598f600e2e670
Amanda Flores <amanda.flores@advendio.com>
2022-12-01 05:05:54 -0300

    pedido obtener

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -22,18 +22,18 @@ exports.crearPedido = async (req, res) => {
 }
 
 exports.obtenerPedidos = async (req, res) => {
-    // try {
-    //     const pedidos = await Pedido.find();
-    //     if (pedidos) {
-    //         res.json(pedidos);
-    //     }
-    //     else{
-    //         res.status(500).send("No existen pedidos");
-    //     }
-    // } catch (error) {
-    //     console.log(error);
-    //     res.status(500).send("hubo un error");
-    // }
+    try {
+        const pedidos = await Pedido.find();
+        if (pedidos) {
+            res.json(pedidos);
+        }
+        else{
+            res.status(500).send("No existen pedidos");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("hubo un error");
+    }
 }
 
 exports.cambiarEstado = async (req, res) => {
